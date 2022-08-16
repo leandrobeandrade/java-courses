fc1b0b2eeedb0722a819f8034b3c32502a712850
leandrobeandrade <leandrobe.andrade@gmail.com>
2022-08-16 16:23:28 -0300

    Java

--- java-course-advanced/file/src/application/Main.java
+++ java-course-advanced/file/src/application/Main.java
@@ -1,16 +1,12 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\leand\\Documents\\Arq\\test.txt");
+		File path = new File("C:\\Users\\leand\\Documents\\Arq\\test.txt");
 //		Scanner scan = null;
 //		
 //		try {
@@ -54,28 +50,51 @@ public class Main {
 //				e.printStackTrace();
 //			}
 //		}
-		
-		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
-		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-			String line = br.readLine();
 
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+//		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
+//		
+//		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+//			for (String line : lines) {
+//				bw.write(line);
+//				bw.newLine();
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+//			String line = br.readLine();
+//
+//			while (line != null) {
+//				System.out.println(line);
+//				line = br.readLine();
+//			}
+//		} catch (IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		}
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a folder path: ");
+		
+		String strPath = scan.nextLine();
+		File path_ = new File(strPath);
+		File[] folders = path_.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
+		
+		File[] files = path_.listFiles(File::isFile);
+		System.out.println("FILES:");
+		
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		scan.close();
 	}
 
 }
