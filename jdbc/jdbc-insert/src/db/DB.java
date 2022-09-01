85e882ee8583d004c9f3b9d4eac93f73952459dc
leandrobeandrade <leandrobe.andrade@gmail.com>
2022-09-01 17:33:46 -0300

    Java

--- jdbc/jdbc-insert/src/db/DB.java
+++ jdbc/jdbc-insert/src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -14,7 +16,7 @@ public class DB {
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
-				Properties props = loadPropertis();
+				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
 			} catch (SQLException e) {
@@ -25,7 +27,7 @@ public class DB {
 		return conn;
 	}
 	
-	private static Properties loadPropertis() {
+	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
@@ -44,6 +46,26 @@ public class DB {
 				throw new DbException(e.getMessage());
 			}
 			
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage()); 
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage()); 
+			}
 		}
 	}
 	
