1e5b352a63f53cff69ded397c3adfaad58abbcba
leandrobeandrade <leandrobe.andrade@gmail.com>
2022-10-04 00:17:50 -0300

    Java

--- javafx/javafx-jdbc/src/gui/util/Utils.java
+++ javafx/javafx-jdbc/src/gui/util/Utils.java
@@ -6,6 +6,16 @@ import javafx.stage.Stage;
 
 public class Utils {
 	public static Stage currentStage(ActionEvent event) {
-		return (Stage) ((Node)event.getSource()).getScene().getWindow();
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+
+	// Não retorna uma exceção mas valores ou null
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);	
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
 }
