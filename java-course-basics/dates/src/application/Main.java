93186ff9337ae62555703f4b757d21c35ecdd0ba
leandrobeandrade <leandrobe.andrade@gmail.com>
2022-10-11 21:11:02 -0300

    Java

--- java-course-basics/dates/src/application/Main.java
+++ java-course-basics/dates/src/application/Main.java
@@ -1,87 +1,12 @@
 package application;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
+import dates.SDFormat;
 
 public class Main {
 
-	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-		Date x1 = new Date();
-		Date x2 = new Date(System.currentTimeMillis());
-		Date x3 = new Date(0L);
-		Date x4 = new Date(1000L * 60L * 60L * 5L);
-		Date y1 = sdf1.parse("25/06/2018");
-		Date y2 = sdf2.parse("25/06/2018 15:42:07");
-		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
-
-		System.out.println("x1: " + x1);
-		System.out.println("x2: " + x2);
-		System.out.println("x3: " + x3);
-		System.out.println("x4: " + x4);
-		System.out.println("y1: " + y1);
-		System.out.println("y2: " + y2);
-		System.out.println("y3: " + y3);
-		System.out.println("-------------");
-
-		System.out.println("x1: " + sdf2.format(x1));
-		System.out.println("x2: " + sdf2.format(x2));
-		System.out.println("x3: " + sdf2.format(x3));
-		System.out.println("x4: " + sdf2.format(x4));
-		System.out.println("y1: " + sdf2.format(y1));
-		System.out.println("y2: " + sdf2.format(y2));
-		System.out.println("y3: " + sdf2.format(y3));
-		System.out.println("-------------");
-
-		System.out.println("x1: " + sdf3.format(x1));
-		System.out.println("x2: " + sdf3.format(x2));
-		System.out.println("x3: " + sdf3.format(x3));
-		System.out.println("x4: " + sdf3.format(x4));
-		System.out.println("y1: " + sdf3.format(y1));
-		System.out.println("y2: " + sdf3.format(y2));
-		System.out.println("y3: " + sdf3.format(y3));
-		System.out.println("-------------");
-
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		Date d = Date.from(Instant.parse("2022-06-23T15:42:07Z"));
-
-		System.out.println(sdf.format(d));
-
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(d);
-
-		int hours = cal.get(Calendar.HOUR_OF_DAY);
-		int minutes = cal.get(Calendar.MINUTE);
-		int seconds = cal.get(Calendar.SECOND);
-
-		int day = cal.get(Calendar.DAY_OF_MONTH);
-		int weekDay = cal.get(Calendar.DAY_OF_WEEK);
-		int month = 1 + cal.get(Calendar.MONTH);
-		int year = cal.get(Calendar.YEAR);
-
-		System.out.println("Hours: " + hours);
-		System.out.println("Minutes: " + minutes);
-		System.out.println("Seconds: " + seconds);
-		System.out.println("Day: " + day);
-		System.out.println("Day of Week: " + weekDay);
-		System.out.println("Month: " + month);
-		System.out.println("Year: " + year);
-		System.out.println("-------------");
-
-		Date data = new Date();
-		Locale local = new Locale("pt", "BR");
-		DateFormat formato = new SimpleDateFormat("dd 'de' MMMM 'de' yyyy", local);
-		System.out.println(formato.format(data));
+	public static void main(String[] args) {
+		SDFormat sdf = new SDFormat();
+		System.out.println(sdf);
 	}
 
 }
