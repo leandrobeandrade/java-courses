1903b1e2828f349682b7c66530ff3bdddfbdae94
leandrobeandrade <leandrobe.andrade@gmail.com>
2022-10-26 22:06:15 -0300

    Java

--- spring-mysql/spring_project/src/main/java/com/projeto/spring_project/entities/OrderItem.java
+++ spring-mysql/spring_project/src/main/java/com/projeto/spring_project/entities/OrderItem.java
@@ -3,11 +3,18 @@ package com.projeto.spring_project.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import com.projeto.spring_project.entities.pk.OrderItemPK;
 
+@Entity
+@Table(name="tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId
 	private OrderItemPK id;
 	private Integer quantity;
 	private Double price;
@@ -15,10 +22,28 @@ public class OrderItem implements Serializable {
 	public OrderItem() {
 	}
 
-	public OrderItem(Integer quantity, Double price) {
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
+		id.setOrder(order);
+		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
 	}
 
 	public Integer getQuantity() {
